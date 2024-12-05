24b0d7bb4ddb704b631151ddb04d6c24ca4f9857
Axel Luna Hernandez <axel.lu04@gmail.com>
2024-12-04 23:52:32 -0600

    Commit para sincronizar

--- ServicioJuego/ImplementacionServicioPartida.cs
+++ ServicioJuego/ImplementacionServicioPartida.cs
@@ -5,7 +5,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel.Design;
 using System.Linq;
 using System.ServiceModel;
-using static ServicioJuego.JugadorPartida;
+//using static ServicioJuego.JugadorPartida;
 
 namespace ServicioJuego
 {
